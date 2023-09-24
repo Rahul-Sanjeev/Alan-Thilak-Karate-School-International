b56f9368588afa31f479f24931f930d30b9a806f
Rahul Sanjeev <rahulsanjeev9@gmail.com>
2023-09-24 23:04:12 +0530

    instructor screen updated

--- alanthilakapp/lib/contants/styles.dart
+++ alanthilakapp/lib/contants/styles.dart
@@ -2,3 +2,8 @@ import 'package:flutter/material.dart';
 
 const TextStyle kEnquryTxtStyle =
     TextStyle(color: Colors.white, fontSize: 21, fontWeight: FontWeight.bold);
+
+const TextStyle kNameTxtStyle = TextStyle(
+    color: Color.fromARGB(255, 255, 255, 76),
+    fontSize: 21,
+    fontWeight: FontWeight.bold);
--- alanthilakapp/lib/screens/Enquiry/Instructors.dart
+++ alanthilakapp/lib/screens/Enquiry/Instructors.dart
@@ -19,150 +19,240 @@ class InstructorsScreen extends StatelessWidget {
         ),
       ),
       body: const SingleChildScrollView(
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Card(
-              color: Colors.grey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      SizedBox(
-                        height: 180,
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 6, top: 6),
-                          child: Image(
-                            image: AssetImage(
-                                '/Users/rahulsanjeev/flutter_projects/alanthilakapp/images/nielmoses.png'),
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Card(
+            color: Colors.grey,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 180,
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 6, top: 6),
+                        child: Image(
+                          image: AssetImage(
+                            '/Users/rahulsanjeev/flutter_projects/alanthilakapp/images/nielmoses.png',
                           ),
                         ),
                       ),
-                      Expanded(
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 9),
-                          child: Text(
-                            'NIEL MOSES THILAK\n\n'
-                            'TECHNICAL DIRECTOR',
-                            style: kEnquryTxtStyle,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(
-                        'Technical Director, Alan Thilak Martial Arts Academy.'
-                        'Joint.Secretary Karate Association of India (KAI)'
-                        'President, Kerala Karate Association'
-                        'Directing Committee Member Kobe Osaka India'
-                        'Shihan Thilak Ha ShitoRyu'),
-                  ),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(
-                        height: 200,
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 6, top: 6),
-                          child: Image(
-                            image: AssetImage(
-                              '/Users/rahulsanjeev/flutter_projects/alanthilakapp/images/sunilkumar.png',
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'NIEL MOSES THILAK',
+                              style: kNameTxtStyle,
                             ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'TECHNICAL DIRECTOR',
+                              style: kEnquryTxtStyle,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Technical Director, Alan Thilak Martial Arts Academy.'
+                    'Joint.Secretary Karate Association of India (KAI)'
+                    'President, Kerala Karate Association'
+                    'Directing Committee Member Kobe Osaka India'
+                    'Shihan Thilak Ha ShitoRyu',
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 180,
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 6, top: 6),
+                        child: Image(
+                          image: AssetImage(
+                            '/Users/rahulsanjeev/flutter_projects/alanthilakapp/images/sunilkumar.png',
                           ),
                         ),
                       ),
-                      Expanded(
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 9),
-                          child: Text(
-                            'P SUNIL KUMAR\n\n'
-                            'TECHNICAL DIRECTOR',
-                            style: kEnquryTxtStyle,
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'P SUNIL KUMAR',
+                              style: kNameTxtStyle,
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'TECHNICAL DIRECTOR',
+                              style: kEnquryTxtStyle,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Vice President, Kerala Karate Association'
+                    'Directing Committee Member Kobe Osaka India'
+                    'Hanshi Thilak Ha ShitoRyu',
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  children: [
+                    SizedBox(
+                      height: 180,
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 6, top: 9),
+                        child: Image(
+                          image: AssetImage(
+                            '/Users/rahulsanjeev/flutter_projects/alanthilakapp/images/anjana.png',
                           ),
                         ),
                       ),
-                    ],
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'ANJANA P KUMAR',
+                              style: kNameTxtStyle,
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'CHIEF INSTRUCTOR',
+                              style: kEnquryTxtStyle,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'CHIEF INSTRUCTOR AT ALAN THILAK KARATE SCHOOL PAYYANNUR & ASIAN KARATE COACH',
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text('WKF Judge'
-                        'Vice President, Kerala Karate Association'
-                        'Directing Committee Member Kobe Osaka India'
-                        'Hanshi Thilak Ha ShitoRyu'),
-                  ),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(
-                        height: 200,
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 6, top: 6),
-                          child: Image(
-                            image: AssetImage(
-                                '/Users/rahulsanjeev/flutter_projects/alanthilakapp/images/anjana.png'),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 180,
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 6, top: 6),
+                        child: Image(
+                          image: AssetImage(
+                            '/Users/rahulsanjeev/flutter_projects/alanthilakapp/images/aryarajesh.png',
                           ),
                         ),
                       ),
-                      Expanded(
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 9),
-                          child: Text(
-                            'ANJANA P KUMAR\n\n'
-                            'CHIEF INSTRUCTOR',
-                            style: kEnquryTxtStyle,
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'AARYA RAJESH',
+                              style: kNameTxtStyle,
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'INSTRUTROR',
+                              style: kEnquryTxtStyle,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'INSTRUCTOR AT ALAN THILAK KARATE SCHOOL PILATHARA BRANCH, NATIONAL PLAYER & MEDALIST',
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 180,
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 6, top: 6),
+                        child: Image(
+                          image: AssetImage(
+                            '/Users/rahulsanjeev/flutter_projects/alanthilakapp/images/praveenkumar.png',
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(
-                        'CHIEF INSTRUCTOR ALANTHILAK KATARE SCHOOL PAYYANNUR'),
-                  ),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(
-                        height: 200,
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 6, top: 6),
-                          child: Image(
-                            image: AssetImage(
-                                '/Users/rahulsanjeev/flutter_projects/alanthilakapp/images/aryarajesh.png'),
-                          ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'PRAVEEN KUMAR',
+                              style: kNameTxtStyle,
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              'INSTRUCTOR',
+                              style: kEnquryTxtStyle,
+                            )
+                          ],
                         ),
                       ),
-                      Expanded(
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 9),
-                          child: Text(
-                            'AARYA RAJESH\n\n'
-                            'INSTRUCTOR\n'
-                            'Pilathara',
-                            style: kEnquryTxtStyle,
-                          ),
-                        ),
-                      ),
-                    ],
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'INSTRUCTOR AT ALAN THILAK KARATE SCHOOL UDINUR BRANCH, KERALA STATE PLAYER & MEDALIST',
                   ),
-                  Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Text(
-                        'INSTRUCTOR AT ALANTHILAK KARATE SCHOOL INTERNATIONAL PILATHARA BRANCH'
-                        'NATIONAL PLAYER AND NATIONAL MEDALIST'),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
